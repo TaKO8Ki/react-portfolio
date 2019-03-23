823195a77c6754bb5489aaf3f1a90d8abd79a678
TaKO8Ki <takoyaki0316@gmail.com>
2019-03-23 13:40:32 +0900

    homeデザイン完成とその他諸々

--- src/App.js
+++ src/App.js
@@ -29,24 +29,30 @@ const App = () => (
 
 const Home = () => (
   <div class="home">
-    
+    <div class="title">Takayuki Maeda's portfolio</div>
+    <div class="buttons">
+    <a href="/works"><div class="name">Works</div></a>
+    <a href="/career"><div class="name">Career</div></a>
+    <a href="/tech"><div class="name">Tech</div></a>
+    </div>
   </div>
 )
 const About = () => (
   <div class="about">
-    <div class="img"><img src="https://i.gyazo.com/c022ae94fdfe4688247e7b2fdd5f283a.jpg"/></div>
-    <div class="content">
-      <h2>前田喬之</h2>
-      <p>神戸大学工学部市民工学科</p>
-      <div class="contact">
-        <div class="each-column">
-          <a href="https://twitter.com/takoyaki3160" target="_blank"><img src={`${process.env.PUBLIC_URL}/twitter.png`} alt="Logo" /></a>
-          <a href="https://www.facebook.com/tako8ki" target="_blank"><img src={`${process.env.PUBLIC_URL}/facebook.png`} alt="Logo" /></a>
-          <a href="https://www.linkedin.com/in/takayuki-maeda/" target="_blank"><img src={`${process.env.PUBLIC_URL}/linkedin.png`} alt="Logo" /></a>
-        </div>
-        <div class="each-column">
-          <a href="https://github.com/TaKO8Ki" target="_blank"><img src={`${process.env.PUBLIC_URL}/github.png`} alt="Logo" /></a>
-          <a href="https://qiita.com/TaKO8Ki" target="_blank"><img src={`${process.env.PUBLIC_URL}/qiita.png`} alt="Logo" /></a>
+    <h2>About</h2>
+    <div class="all-content">
+      <div class="img"><img src="https://i.gyazo.com/c022ae94fdfe4688247e7b2fdd5f283a.jpg"/></div>
+      <div class="content">
+        <div class="name">前田喬之</div>
+        <p>神戸大学工学部市民工学科</p>
+        <div class="contact">
+          <div class="each-column">
+            <a href="https://twitter.com/takoyaki3160" target="_blank" class="twitter"><img src={`${process.env.PUBLIC_URL}/twitter.png`} alt="Logo" /></a>
+            <a href="https://www.facebook.com/tako8ki" target="_blank"><img src={`${process.env.PUBLIC_URL}/facebook.png`} alt="Logo" /></a>
+            <a href="https://www.linkedin.com/in/takayuki-maeda/" target="_blank"><img src={`${process.env.PUBLIC_URL}/linkedin.png`} alt="Logo" /></a>
+            <a href="https://github.com/TaKO8Ki" target="_blank"><img src={`${process.env.PUBLIC_URL}/github.png`} alt="Logo" /></a>
+            <a href="https://qiita.com/TaKO8Ki" target="_blank"><img src={`${process.env.PUBLIC_URL}/qiita.png`} alt="Logo" /></a>
+          </div>
         </div>
       </div>
     </div>
@@ -63,6 +69,7 @@ const Works = () => (
           </div>
           <div class="description">
             「助け合って検索する」をテーマにしたサービスLinch（リンク）です。
+            Qiitaでサービスの紹介記事を書いたので、そちらをみていただくと分かりやすいと思います。
             <div class="link">
               <a href="https://linch.link/" target="_blank">Linch</a>
               <a href="https://qiita.com/TaKO8Ki/items/fd6c39d67d8b32c3d47c" target="_blank">紹介記事</a>
@@ -81,8 +88,15 @@ const Works = () => (
             </div>
           </div>
         </div>
-        <div class="lack">
-          comming soon
+        <div class="content">
+          <div class="title">ポートフォリオ</div>
+          <div class="description">
+            フロントエンドはほとんど手をつけていなかったので、
+            このポートフォリオを作るために少し勉強しました。
+            <div class="link">
+              <a href="https://tako8ki-portfolio.herokuapp.com/" target="_blank">ポートフォリオ</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -93,6 +107,10 @@ const Career = () => (
   <div class="career">
     <h2>Career</h2>
     <div class="all-careers">
+    <div class="title">神戸大学工学部市民工学科（2021年卒業予定）</div>
+      <div class="description">
+        市民工学科という学科で、土木工学について勉強しています。
+      </div>
       <div class="title">株式会社activo（2018年6月から現在まで）</div>
       <div class="description">
         サーバーサイドエンジニアとして自社サービス「activo」の開発を行う長期インターンをしています。
@@ -124,6 +142,7 @@ const Tech = () => (
           <div class="title">Ruby・Rails</div>
           <div class="description">
             インターンや個人開発でメインとして使っています。
+            業務では、Rspecを使ってテストを書くこともよくあります。
             実際に作ったサービスがあるので、そちらをみていただけると分かりやすいと思います。
           </div>
           <div class="date">使用歴 : {Today.diff(Begin＿Ruby＿At, "month")}ヶ月</div>

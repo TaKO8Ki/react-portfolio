1079f41c7dbfc71b595348399bd4db4aebcc01d6
TaKO8Ki <takoyaki0316@gmail.com>
2019-09-10 01:00:48 +0900

    test commit

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ class Home extends React.Component {
   render() {
     return (
       <div class="home">
-        <div class="title">Takayuki Maeda's portfolio</div>
+        <div class="title">Takayuki Maeda's portfoliohogeghewogiheoih</div>
         <div class="buttons">
         <Link to='/works'><div class="name">Works</div></Link>
         <Link to='/career'><div class="name">Career</div></Link>

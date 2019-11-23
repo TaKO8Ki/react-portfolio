e5f5045a42296232d92f61555c9ff556ba6f0a25
TaKO8Ki <takoyaki0316@gmail.com>
2019-11-23 22:36:40 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 ssh-keyscan -H 133.130.122.93 >> ~/.ssh/known_hosts
-chown -R runner:runner ~/.ssh/
 sed -i -e "s#\\\\n#\n#g" ~/.ssh/id_rsa
-ssh TaKO8Ki@133.130.122.93 -i ~/.ssh/id_rsa <<EOF
+sed -i -e "s#\\\\n#\n#g" ~/.ssh/id_rsa.pub
+ssh -t -t TaKO8Ki@133.130.122.93 -i ~/.ssh/id_rsa <<EOF
 echo success
 exit
 EOF

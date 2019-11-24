32e4a9886d8b6f18a9d972384096d8fe65a1173d
TaKO8Ki <takoyaki0316@gmail.com>
2019-11-25 01:29:40 +0900

    Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,3 @@
-docker build -t takoyaki0316/react-portfolio:latest -f /scripts/Dockerfile .
+docker build -t takoyaki0316/react-portfolio:latest -f ./scripts/Dockerfile .
 docker images
 docker push takoyaki0316/react-portfolio:latest

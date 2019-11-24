dc921aa7aee5081bc716bedde3b9accb89f73c17
TaKO8Ki <takoyaki0316@gmail.com>
2019-11-25 03:31:04 +0900

    fix title

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
         <div>
         <div class="header">
           <div class="content">
-            <Link to='/'>Emoh</Link>
+            <Link to='/'>Home</Link>
             <Link to='/about'>About</Link>
             <Link to='/works'>Works</Link>
             <Link to='/career'>Career</Link>
